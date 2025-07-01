4e7d5aa0a1db18f8bbb51341d4671d90a574a0fb
Pavel Rubtsov <prubtcov@gmail.com>
2025-07-01 18:51:58 +0300

    Revert "Delete build directory"
    This reverts commit b78a8ffc49f97e5638eecfb8e64141e99268c4e3.

---
+++ build/CMakeFiles/cv_to_osc_converter.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cv_to_osc_converter.dir/AudioDeviceManager.cpp.o"
+  "CMakeFiles/cv_to_osc_converter.dir/AudioDeviceManager.cpp.o.d"
+  "CMakeFiles/cv_to_osc_converter.dir/CVReader.cpp.o"
+  "CMakeFiles/cv_to_osc_converter.dir/CVReader.cpp.o.d"
+  "CMakeFiles/cv_to_osc_converter.dir/CommandLineInterface.cpp.o"
+  "CMakeFiles/cv_to_osc_converter.dir/CommandLineInterface.cpp.o.d"
+  "CMakeFiles/cv_to_osc_converter.dir/OSCSender.cpp.o"
+  "CMakeFiles/cv_to_osc_converter.dir/OSCSender.cpp.o.d"
+  "CMakeFiles/cv_to_osc_converter.dir/main.cpp.o"
+  "CMakeFiles/cv_to_osc_converter.dir/main.cpp.o.d"
+  "cv_to_osc_converter"
+  "cv_to_osc_converter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cv_to_osc_converter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/cv_to_osc_converter.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cv_to_osc_converter.
+# This may be replaced when dependencies are built.
